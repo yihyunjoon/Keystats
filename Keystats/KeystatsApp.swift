0bb3f8ffa009ea605ec5920c3dd02b731215247d
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2025-12-29 15:13:37 +0900

    Initial Commit

---
+++ Keystats/KeystatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeystatsApp.swift
+//  Keystats
+//
+//  Created by yihyunjoon on 2025-12-29.
+//
+
+import SwiftUI
+
+@main
+struct KeystatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
